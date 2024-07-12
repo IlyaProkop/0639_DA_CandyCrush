6c526994ad8a3540e6a1a12c5ae6a6e3ffab7d65
IlyaProkop <ilya.prokopchik.05@mail.ru>
2024-07-12 15:20:02 +0300

    init

---
+++ Assets/Scripts/Board/NormalItem.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalItem : Item
+{
+    public enum eNormalType
+    {
+        TYPE_ONE,
+        TYPE_TWO,
+        TYPE_THREE,
+        TYPE_FOUR,
+        TYPE_FIVE,
+        TYPE_SIX,
+        TYPE_SEVEN
+    }
+
+    public eNormalType ItemType;
+
+    public void SetType(eNormalType type)
+    {
+        ItemType = type;
+    }
+
+    protected override string GetPrefabName()
+    {
+        string prefabname = string.Empty;
+        switch (ItemType)
+        {
+            case eNormalType.TYPE_ONE:
+                prefabname = Constants.PREFAB_NORMAL_TYPE_ONE;
+                break;
+            case eNormalType.TYPE_TWO:
+                prefabname = Constants.PREFAB_NORMAL_TYPE_TWO;
+                break;
+            case eNormalType.TYPE_THREE:
+                prefabname = Constants.PREFAB_NORMAL_TYPE_THREE;
+                break;
+            case eNormalType.TYPE_FOUR:
+                prefabname = Constants.PREFAB_NORMAL_TYPE_FOUR;
+                break;
+            case eNormalType.TYPE_FIVE:
+                prefabname = Constants.PREFAB_NORMAL_TYPE_FIVE;
+                break;
+            case eNormalType.TYPE_SIX:
+                prefabname = Constants.PREFAB_NORMAL_TYPE_SIX;
+                break;
+            case eNormalType.TYPE_SEVEN:
+                prefabname = Constants.PREFAB_NORMAL_TYPE_SEVEN;
+                break;
+        }
+
+        return prefabname;
+    }
+
+    internal override bool IsSameType(Item other)
+    {
+        NormalItem it = other as NormalItem;
+
+        return it != null && it.ItemType == this.ItemType;
+    }
+
+    public int GetIndexSprite()
+    {
+        int index = -1;
+        switch (ItemType)
+        {
+            case eNormalType.TYPE_ONE:
+                index = 0;
+                break;
+            case eNormalType.TYPE_TWO:
+                index = 1;
+                break;
+            case eNormalType.TYPE_THREE:
+                index = 2;
+                break;
+            case eNormalType.TYPE_FOUR:
+                index = 3;
+                break;
+            case eNormalType.TYPE_FIVE:
+                index = 4;
+                break;
+            case eNormalType.TYPE_SIX:
+                index = 5;
+                break;
+            case eNormalType.TYPE_SEVEN:
+                index = 6;
+                break;
+        }
+
+        return index;
+    }
+}
---
+++ Assets/Scripts/Controllers/GameManager.cs
@@ -0,0 +1,160 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public event Action<eStateGame> StateChangedAction = delegate { };
+
+    public enum eLevelMode
+    {
+        TIMER,
+        MOVES
+    }
+
+    public enum eStateGame
+    {
+        SETUP,
+        MAIN_MENU,
+        GAME_STARTED,
+        PAUSE,
+        GAME_OVER,
+    }
+
+    private eStateGame m_state;
+    public eStateGame State
+    {
+        get { return m_state; }
+        private set
+        {
+            m_state = value;
+
+            StateChangedAction(m_state);
+        }
+    }
+
+
+    private GameSettings m_gameSettings;
+
+
+    private BoardController m_boardController;
+
+    private UIMainManager m_uiMenu;
+
+    private LevelCondition m_levelCondition;
+
+    private SkinItemManagerSO m_skinItemManager;
+
+    [SerializeField] TypeSkin typeSkin;
+
+    private void Awake()
+    {
+        State = eStateGame.SETUP;
+
+        m_gameSettings = Resources.Load<GameSettings>(Constants.GAME_SETTINGS_PATH);
+
+        m_uiMenu = FindObjectOfType<UIMainManager>();
+        m_uiMenu.Setup(this);
+
+        m_skinItemManager = Resources.Load<SkinItemManagerSO>(Constants.SKIN_ITEM_MANAGER_PATH);
+        foreach(var skinItem in m_skinItemManager.skinItems)
+        {
+            if(skinItem.type == typeSkin)
+            {
+                for(int i = 0; i < skinItem.sprites.Length; i++)
+                {
+                    GameObject prefab = Resources.Load<GameObject>($"{Constants.ITEM_PREFAB_PATH}{( i + 1 )}");
+                    if(prefab)
+                    {
+                        prefab.GetComponent<SpriteRenderer>().sprite = skinItem.sprites[i];
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    void Start()
+    {
+        State = eStateGame.MAIN_MENU;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_boardController != null) m_boardController.Update();
+    }
+
+
+    internal void SetState(eStateGame state)
+    {
+        State = state;
+
+        if(State == eStateGame.PAUSE)
+        {
+            DOTween.PauseAll();
+        }
+        else
+        {
+            DOTween.PlayAll();
+        }
+    }
+
+    public void LoadLevel(eLevelMode mode)
+    {
+        m_boardController = new GameObject("BoardController").AddComponent<BoardController>();
+        m_boardController.StartGame(this, m_gameSettings);
+
+        if (mode == eLevelMode.MOVES)
+        {
+            m_levelCondition = this.gameObject.AddComponent<LevelMoves>();
+            m_levelCondition.Setup(m_gameSettings.LevelMoves, m_uiMenu.GetLevelConditionView(), m_boardController);
+        }
+        else if (mode == eLevelMode.TIMER)
+        {
+            m_levelCondition = this.gameObject.AddComponent<LevelTime>();
+            m_levelCondition.Setup(m_gameSettings.LevelTime, m_uiMenu.GetLevelConditionView(), this);
+        }
+
+        m_levelCondition.ConditionCompleteEvent += GameOver;
+
+        State = eStateGame.GAME_STARTED;
+    }
+
+    public void GameOver()
+    {
+        StartCoroutine(WaitBoardController());
+    }
+
+    internal void ClearLevel()
+    {
+        if (m_boardController)
+        {
+            m_boardController.Clear();
+            Destroy(m_boardController.gameObject);
+            m_boardController = null;
+        }
+    }
+
+    private IEnumerator WaitBoardController()
+    {
+        while (m_boardController.IsBusy)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        State = eStateGame.GAME_OVER;
+
+        if (m_levelCondition != null)
+        {
+            m_levelCondition.ConditionCompleteEvent -= GameOver;
+
+            Destroy(m_levelCondition);
+            m_levelCondition = null;
+        }
+    }
+}
